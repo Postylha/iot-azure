a0482103765d7d70c5ca0f08de1e00cc369ff0af
Postylha <114649888+Postylha@users.noreply.github.com>
2022-11-10 09:58:16 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,5 +1,7 @@
 <form action="index4.php" method="post">
     <div>
+        <label for="name">Name:</label>
+        <input type="name" id="name" name="name" />
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" />
     </div>
