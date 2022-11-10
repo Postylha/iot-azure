b98162041dead4ff358dd98bee1954d26f04fd58
Postylha <114649888+Postylha@users.noreply.github.com>
2022-11-10 09:51:05 +0100

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,7 @@
+<form action="index.php" method="post">
+    <div>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" />
+    </div>
+    <button type="submit">Submit</button>
+</form>
