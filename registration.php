bf144b4409f53440a62f4ebed2d0a7acc70a39f0
Postylha <114649888+Postylha@users.noreply.github.com>
2022-11-15 00:39:51 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,3 +1,8 @@
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    
+</head>
+<body>
 <form action="index4.php" method="post">
     <div>
         <label for="name">Name:</label>
@@ -7,3 +12,4 @@
     </div>
     <button type="submit">Submit</button>
 </form>
+</body>
