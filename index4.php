e85a5a163085966509dd11ad8a1b3827659ac1e8
Postylha <114649888+Postylha@users.noreply.github.com>
2022-11-15 20:37:09 +0100

    Update index4.php

--- index4.php
+++ index4.php
@@ -1,20 +1,26 @@
 <?php
-  echo '<h1>Params page</h1>';
-  
-  $a = $_GET["a"];
-  $b = $_GET["b"];
-  
-  $c = $a+$b;
-  
-  echo "sum of params = $c";
-
   if (isset($_POST['fname'], $_POST['email'])) {
 	$fname = htmlspecialchars($_POST['fname']);
+	$lname = htmlspecialchars($_POST['lname']);
+	
 	$email = htmlspecialchars($_POST['email']);
 
 	// show the $name and $email
-	echo "Thanks $name for your subscription.<br>";
-	echo "Please confirm it in your inbox of the email $email.";
+	echo "Thanks $fname for your subscription.<br>";
+	echo "Please check your inbox $email for futher instruction.";
+	
+
+	$file1 = fopen("main.txt","w") or die("Unable to open file!");
+	$text1 = "firstName=" . $fname . "lastName=" . $lname . "email=" . $email;
+
+	fwrite($file1, $text1);
+	fclose($file1);
+	
+	$file2 = fopen("additional.txt","w") or die("Unable to open file!");
+	$text1 = "phone=" . htmlspecialchars($_POST['phone']);
+	
+	fwrite($file2, $text1);
+	fclose($file2);
 } else {
 	echo 'You need to provide your name and email address.';
 }
