995a8244d1162dbf02f384eebd55b0be3aa08814
Postylha <114649888+Postylha@users.noreply.github.com>
2022-11-15 23:55:52 +0100

    Update index4.php

--- index4.php
+++ index4.php
@@ -1,3 +1,14 @@
+<html>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<style>
+		body {
+			background-color:#ECF1F9;
+		}
+	</style>
+</head>
+<body>
 <?php
   
   if (isset($_POST['fname'], $_POST['email'])) {
@@ -7,8 +18,8 @@
 	$email = htmlspecialchars($_POST['email']);
 
 	// show the $name and $email
-	echo '<h1 class="display-3">Thanks $fname for your subscription.<br>';
-	echo 'Please check your inbox $email for futher instruction.</h1>';
+	echo "<h1 class=\"display-3\">Thanks $fname for your subscription.<br>";
+	echo "Please check your inbox $email for futher instruction.</h1>";
 	
 
 	$file1 = fopen("main.txt","a") or die("Unable to open file!");
@@ -26,3 +37,5 @@
 		echo '<h1 class="display-4">You need to provide your name and email address.</h1>';
   }
 ?>
+</body>
+</html>
