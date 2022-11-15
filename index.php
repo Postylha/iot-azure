dc25b477d3c463dc8885d5b8fbcfd3b6af47d650
Postylha <114649888+Postylha@users.noreply.github.com>
2022-11-15 21:23:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php echo '<h1>hello, druzija!</h1>'; ?>
+<head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    body {
+			background-color:#ECF1F9;
+		}
+</head>
+<body>
+  <h1 class="display-4">Index</p>
+  <a href="registration.php" class="link-secondary">Click here to register</a>
+</body>
