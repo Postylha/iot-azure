f00d3377e9009e2b4d565d9d50f2039452eef7dd
Postylha <114649888+Postylha@users.noreply.github.com>
2022-11-15 21:33:49 +0100

    Update index4.php

--- index4.php
+++ index4.php
@@ -1,3 +1,14 @@
+<html>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+	<style>
+		body {
+			background-color:#ECF1F9;
+		}
+		
+	</style>
+</head>
+<body>
 <?php
   if (isset($_POST['fname'], $_POST['email'])) {
 	$fname = htmlspecialchars($_POST['fname']);
@@ -6,23 +17,25 @@
 	$email = htmlspecialchars($_POST['email']);
 
 	// show the $name and $email
-	echo "Thanks $fname for your subscription.<br>";
-	echo "Please check your inbox $email for futher instruction.";
+	echo "<h1 class="display-1">Thanks $fname for your subscription.<br>";
+	echo "Please check your inbox $email for futher instruction.</h1>";
 	
 
 	$file1 = fopen("main.txt","a") or die("Unable to open file!");
 	$text1 = "firstName=" . $fname . " lastName=" . $lname . " email=" . $email;
 
-	fwrite($file1, $text1);
+	fwrite($file1, "\n". $text1);
 	fclose($file1);
 	
 	$file2 = fopen("additional.txt","a") or die("Unable to open file!");
 	$text1 = "phone=" . htmlspecialchars($_POST['phone']) . " birthday=" . htmlspecialchars($_POST['birthday']) . " information=" . htmlspecialchars($_POST['information']);
 	
-	fwrite($file2, $text1);
+	fwrite($file2, "\n". $text1);
 	fclose($file2);
 } else {
-	echo 'You need to provide your name and email address.';
+	echo '<h1>You need to provide your name and email address.</h1>';
 }
 
 ?>
+</body>
+</html>
