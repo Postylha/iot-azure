5cdcf61025b419946cf374afc5f4f18406ca083a
Postylha <114649888+Postylha@users.noreply.github.com>
2022-11-15 21:24:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <head>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    body {
+    <style>
+	    body {
 			background-color:#ECF1F9;
 		}
+	</style>	    
+	    
 </head>
 <body>
   <h1 class="display-4">Index</p>
