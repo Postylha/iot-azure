6a11d46983d2b10aef6e44d107692fdfd341a232
Postylha <114649888+Postylha@users.noreply.github.com>
2022-11-15 22:09:25 +0100

    Update index4.php

--- index4.php
+++ index4.php
@@ -1,5 +1,4 @@
-<?php echo <<<EOD
-<html>
+<?php echo '<html>
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 	<style>
@@ -9,8 +8,7 @@
 		
 	</style>
 </head>
-<body>
-EOD;
+<body>';
 ?>
 <?php
   if (isset($_POST['fname'], $_POST['email'])) {
