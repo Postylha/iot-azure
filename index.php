3a08607de6df15c08f7dbd884b26a64418b8a067
Postylha <114649888+Postylha@users.noreply.github.com>
2022-11-15 21:28:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
+<html>
 <head>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <style>
-	    body {
-			background-color:#ECF1F9;
-		}
-	</style>	    
+  <style>
+    body {
+      background-color:#ECF1F9;
+    }
+  </style>	    
 	    
 </head>
 <body>
   <h1 class="display-4">Index</h1>
   <a href="registration.php" class="link-secondary">Click here to register</a>
 </body>
+</html>
