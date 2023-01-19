8f47fbd5853eacd362a70b102d992afa374e2bd3
Postylha <114649888+Postylha@users.noreply.github.com>
2023-01-19 14:24:28 +0100

    Update exam.php

--- exam.php
+++ exam.php
@@ -1,7 +1,11 @@
 <?php
-     echo 'new server message';
+     
      if (!empty($_POST))
      {
           echo $_POST[0];    
+          echo 'server message';
+     } else {
+          echo 'prazdna stranka';
      }
+
 ?>
