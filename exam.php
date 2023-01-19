2c7845a0377f96fdd55d548841dfa5699764f4af
Postylha <114649888+Postylha@users.noreply.github.com>
2023-01-19 14:37:50 +0100

    Update exam.php

--- exam.php
+++ exam.php
@@ -3,6 +3,10 @@
      if (isset($_POST['h']))
      {
           echo $_POST['h'];    
+          $file1 = fopen("exam.txt","a") or die("Unable to open file!");
+          fwrite($file1, "\n". $_POST['h']);
+	     fclose($file1);
+          
           echo 'server message';
      } else {
           echo 'prazdna stranka';
