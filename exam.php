7ae9ff23fbbc28846d37d92beb4c7dd90efdd00f
Postylha <114649888+Postylha@users.noreply.github.com>
2023-01-19 13:51:10 +0100

    Create exam.php

---
+++ exam.php
@@ -0,0 +1,9 @@
+<?php
+        if(isset($_POST['page']) {
+                if($_POST['page'] == 1) {
+                        echo '1 page';
+                } else {
+                        echo 'pages';
+                }
+        }
+?>
